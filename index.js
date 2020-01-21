8f5957e76ee3de1c1058dd46192e3a500d24850d
Igor Veyner <igorveyner95@gmail.com>
2020-01-21 20:37:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(mom){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(Christmas, mom){
+  return `Happy ${holiday}, ${name}!`
+}
